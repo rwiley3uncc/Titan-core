6d63193a18912f21b59f3cc726b8de5902979450
Ron Wiley <rwiley3@charlotte.edu>
2026-05-05 18:30:13 -0400

    update html

--- run_titan.ps1
+++ run_titan.ps1
@@ -1,4 +1,6 @@
 # Compatibility launcher for desktop shortcuts.
 # Calls the primary Titan start script.
 
+Set-Location "C:\Users\mouse\DEV\Titan-core"
+
 & "$PSScriptRoot\start_titan.ps1"
--- start_titan.ps1
+++ start_titan.ps1
@@ -9,7 +9,7 @@ if (-not $repoRoot) {
 
 Set-Location $repoRoot
 
-$uiUrl = "http://127.0.0.1:8000/ui/index.html?fresh=30"
+$uiUrl = "http://127.0.0.1:8000/ui/index.html"
 
 # CHANGE THIS if your Ubuntu web server command is different
 $wslDistro = "Ubuntu"
